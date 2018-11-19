f0072c2face20aada9c3333eb86c8ed2d690c9d1
SchawnnDev <iperskill@gmail.com>
2018-11-20 00:02:55 +0100

    starting project

---
+++ CaisseServer/CaisseServerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaisseServer
+{
+    public class CaisseServerContext : DbContext
+    {
+
+        public CaisseServerContext() : base("name=ConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
