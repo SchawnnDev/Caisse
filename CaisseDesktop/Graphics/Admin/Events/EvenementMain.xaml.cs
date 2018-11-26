5fb4af250524fc11e487e98c9131cd77b77d98cc
SchawnnDev <iperskill@gmail.com>
2018-11-26 11:55:04 +0100

    start event

---
+++ CaisseDesktop/Graphics/Admin/Events/EvenementMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CaisseServer;
+using CaisseServer.Events;
+
+namespace CaisseDesktop.Graphics.Admin.Events
+{
+    /// <summary>
+    /// Interaction logic for EvenementMain.xaml
+    /// </summary>
+    public partial class EvenementMain : Window
+    {
+        public List<SaveableEvent> Events { get; }
+
+        public EvenementMain()
+        {
+            InitializeComponent();
+
+            using (var db = new CaisseServerContext())
+                Events = db.Events.OrderBy(e => e.End).ToList();
+        }
+    }
+}
--- CaisseServer/Events/SaveableEvent.cs
+++ CaisseServer/Events/SaveableEvent.cs
@@ -12,9 +12,15 @@ namespace CaisseServer.Events
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public DateTime Start { get; set; }
 
         public DateTime End { get; set; }
+
+        public string Addresse { get; set; }
+
+        public string Description { get; set; }
 
         public SaveableEvent()
         {
