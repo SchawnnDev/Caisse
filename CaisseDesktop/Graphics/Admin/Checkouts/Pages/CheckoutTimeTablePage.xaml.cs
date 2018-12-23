e54f324924406ccd801c11f40ae4f6b5517ae2ef
SchawnnDev <iperskill@gmail.com>
2018-12-24 00:57:58 +0100

    Time table

--- CaisseDesktop/Graphics/Admin/Checkouts/Pages/CheckoutTimeTablePage.xaml.cs
+++ CaisseDesktop/Graphics/Admin/Checkouts/Pages/CheckoutTimeTablePage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CaisseDesktop.Enums;
+using CaisseServer.Events;
 
 namespace CaisseDesktop.Graphics.Admin.Checkouts.Pages
 {
@@ -23,6 +25,51 @@ namespace CaisseDesktop.Graphics.Admin.Checkouts.Pages
         public CheckoutTimeTablePage()
         {
             InitializeComponent();
+
+            var day = new SaveableDay
+            {
+                Start = new DateTime(2018, 12, 23, 8, 00, 00),
+                End = new DateTime(2018, 12, 23, 17, 00, 00)
+            };
+
+            var day2 = new SaveableDay
+            {
+                Start = new DateTime(2018, 12, 24, 08, 00, 00),
+                End = new DateTime(2018, 12, 24, 17, 00, 00)
+            };
+
+            var day3 = new SaveableDay
+            {
+                Start = new DateTime(2018, 12, 25, 08, 00, 00),
+                End = new DateTime(2018, 12, 25, 17, 00, 00)
+            };
+
+            var list = new List<SaveableTimeSlot>
+            {
+                new SaveableTimeSlot
+                {
+                    Day = day,
+                    Start = new DateTime(2018, 12, 23, 10, 00, 00),
+                    End = new DateTime(2018, 12, 23, 12, 00, 00)
+                },
+                new SaveableTimeSlot
+                {
+                    Day = day,
+                    Start = new DateTime(2018, 12, 23, 14, 00, 00),
+                    End = new DateTime(2018, 12, 23, 15, 00, 00)
+                },
+                new SaveableTimeSlot
+                {
+                    Day = day,
+                    Start = new DateTime(2018, 12, 23, 15, 00, 00),
+                    End = new DateTime(2018, 12, 23, 16, 00, 00)
+                },
+            };
+
+            Fill(TimeTableDay.DayOne, day, list);
+            Fill(TimeTableDay.DayTwo, day2, new List<SaveableTimeSlot>());
+            Fill(TimeTableDay.DayThree, day3, new List<SaveableTimeSlot>());
+
         }
 
         public override void Update()
@@ -43,6 +90,74 @@ namespace CaisseDesktop.Graphics.Admin.Checkouts.Pages
         public override bool CanBack()
         {
             throw new NotImplementedException();
+        }
+
+        public void Fill(TimeTableDay timeTableDay, SaveableDay day, List<SaveableTimeSlot> slots)
+        {
+            DockPanel panel;
+            Brush brush;
+            switch (timeTableDay)
+            {
+                case TimeTableDay.DayOne:
+                    panel = Day1;
+                    brush = Brushes.DarkSeaGreen;
+                    break;
+                case TimeTableDay.DayTwo:
+                    panel = Day2;
+                    brush = Brushes.CornflowerBlue;
+                    break;
+                case TimeTableDay.DayThree:
+                    panel = Day3;
+                    brush = Brushes.DarkSalmon;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(timeTableDay), timeTableDay, null);
+            }
+
+            //TimeTableGrid.ColumnDefinitions[1].
+
+            var label = new Button
+            {
+                Content = day.Start.ToString("D"),
+                Background = brush
+            };
+
+            DockPanel.SetDock(label, Dock.Top);
+            panel.Children.Add(label);
+
+            if (slots.Count == 0)
+            {
+                var dayBtn = new Button
+                {
+                    Content = $"{day.Start.Date:T}\n\n\n{day.Start.Date:d}\n\n\n{day.End.Date:T}",
+                    Background = brush,
+                    Height = double.NaN
+                };
+
+                DockPanel.SetDock(dayBtn, Dock.Top);
+
+                panel.DataContext = day;
+                panel.Children.Add(dayBtn);
+
+                return;
+
+            }
+
+            foreach (var slot in slots)
+            {
+                var dayBtn = new Button
+                {
+                    Content = $"{slot.Start.Date:T}\n\n\n{slot.End.Date:T}",
+                    Background = brush,
+                    Height = double.NaN
+                };
+
+                DockPanel.SetDock(dayBtn, Dock.Top);
+
+                panel.DataContext = day;
+                panel.Children.Add(dayBtn);
+            }
+
         }
 
         public override string CustomName => "CheckoutTimeTablePage";
