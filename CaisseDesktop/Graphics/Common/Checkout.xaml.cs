8000a924ac9f2743e4ef746b6720f7aef92fccec
SchawnnDev <iperskill@gmail.com>
2018-12-26 21:15:03 +0100

    Minimalist design

--- CaisseDesktop/Graphics/Common/Checkout.xaml.cs
+++ CaisseDesktop/Graphics/Common/Checkout.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CaisseServer;
 
 namespace CaisseDesktop.Graphics.Common
 {
@@ -22,6 +24,114 @@ namespace CaisseDesktop.Graphics.Common
         public Checkout()
         {
             InitializeComponent();
+            CreateItemGrid(new List<SaveableItem>
+            {
+                new SaveableItem
+                {
+                    ImageSrc = ""
+                }, new SaveableItem
+                {
+
+                }, new SaveableItem
+                {
+
+                }, new SaveableItem
+                {
+
+                }, new SaveableItem
+                {
+
+                },
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateItemGrid(List<SaveableItem> items)
+        {
+            var panel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+
+            foreach (var item in items)
+                panel.Children.Add(CreateIten(item));
+
+            MainGrid.Children.Add(panel);
+
+        }
+
+        private Border CreateIten(SaveableItem item)
+        {
+            var border = new Border
+            {
+                BorderThickness = new Thickness(1.0),
+                BorderBrush = Brushes.DimGray,
+                Margin = new Thickness(50.0)
+            };
+
+            var panel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Margin = new Thickness(5.0)
+            };
+
+            var label = new Label
+            {
+                FontSize = 20.0,
+                Content = item.Name,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Foreground = Brushes.White
+            };
+
+            var img = new Image
+            {
+                MaxWidth = 300.0,
+                MaxHeight = 55.0
+            };
+
+            var stackPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            var minusBtn = new Button
+            {
+                Content = "-"
+            };
+
+            var textBox = new TextBox
+            {
+                Text = "0"
+            };
+
+            var plusBtn = new Button
+            {
+                Content = "+"
+            };
+
+            stackPanel.Children.Add(minusBtn);
+            stackPanel.Children.Add(textBox);
+            stackPanel.Children.Add(plusBtn);
+
+            var remainingPanel = new Label
+            {
+                Content = "100 restants"
+            };
+
+            panel.Children.Add(label);
+            panel.Children.Add(img);
+            panel.Children.Add(stackPanel);
+            panel.Children.Add(remainingPanel);
+
+            border.Child = panel;
+
+            return border;
         }
     }
 }
--- CaisseServer/SaveableItem.cs
+++ CaisseServer/SaveableItem.cs
@@ -15,6 +15,14 @@ namespace CaisseServer
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        public SaveableCheckoutType Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageSrc { get; set; }
+
+        public decimal Price { get; set; }
+
         public SaveableItem()
         {
 
