4b3d303546efa6af92c2a8870e900916a915c551
SchawnnDev <iperskill@gmail.com>
2019-04-02 20:29:20 +0200

    time slots

---
+++ CaisseDesktop/Graphics/Admin/TimeSlots/TimeSlotManager.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CaisseDesktop.Graphics.Admin.TimeSlots
+{
+    /// <summary>
+    /// Interaction logic for TimeSlotManager.xaml
+    /// </summary>
+    public partial class TimeSlotManager : Window
+    {
+        public TimeSlotManager()
+        {
+            InitializeComponent();
+        }
+
+        private void TimeSlotCashier_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
