7316e019f3f823cd8297146f7fa0d3de2f5edabf
SchawnnDev <iperskill@gmail.com>
2019-05-09 15:25:36 +0200

    Wow, that's magic!

--- CaisseDesktop/Graphics/Admin/Events/Pages/EventMainPage.xaml.cs
+++ CaisseDesktop/Graphics/Admin/Events/Pages/EventMainPage.xaml.cs
@@ -12,167 +12,116 @@ using MaterialDesignThemes.Wpf;
 
 namespace CaisseDesktop.Graphics.Admin.Events.Pages
 {
-    /// <summary>
-    ///     Interaction logic for MainEventManager.xaml
-    /// </summary>
-    public partial class EventMainPage
-    {
-        public EventMainPage(EvenementManager parentWindow)
-        {
-            InitializeComponent();
-            ParentWindow = parentWindow;
+	/// <summary>
+	///     Interaction logic for MainEventManager.xaml
+	/// </summary>
+	public partial class EventMainPage
+	{
+		public EventMainPage(EvenementManager parentWindow)
+		{
+			InitializeComponent();
+			ParentWindow = parentWindow;
+			DataContext = new EventMainPageModel(parentWindow.Evenement);
+		}
 
-            if (parentWindow.Evenement != null)
-            {
-                FillTextBoxes();
-                New = false;
-                Saved = true;
-                ToggleBlocked(true);
-            }
-            else
-            {
-                Blocage.IsChecked = false;
-            }
-        }
+		private bool New { get; } = true;
+		private bool Saved { get; set; }
+		private bool FirstClose { get; set; } = true;
+		private EvenementManager ParentWindow { get; }
+		private EventMainPageModel Model => DataContext as EventMainPageModel;
+		public override string CustomName => "EventMainPage";
 
-        private bool New { get; } = true;
-        private bool Saved { get; set; }
-        private bool Blocked { get; set; }
-	    private bool FirstClose { get; set; } = true;
-        private EvenementManager ParentWindow { get; }
-
-        public override string CustomName => "EventMainPage";
-
-        private void ToggleBlocked(bool blocked)
-        {
-            EventName.IsEnabled = !blocked;
-           // EventAddresse.IsEnabled = !blocked;
-            EventDescription.IsEnabled = !blocked;
-            EventStartButton.IsEnabled = !blocked;
-            EventEndButton.IsEnabled = !blocked;
-            EventSave.IsEnabled = !blocked;
-            Blocage.IsChecked = blocked;
-            Blocked = blocked;
-        }
-
-        private void FillTextBoxes()
-        {
-            EventName.Text = ParentWindow.Evenement.Name;
-           // EventStart.Value = ParentWindow.Evenement.Start;
-           // EventEnd.Value = ParentWindow.Evenement.End;
-            EventDescription.Text = ParentWindow.Evenement.Description;
-           // EventAddresse.Text = ParentWindow.Evenement.Address;
-        }
-
-        private void Save_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (Check(EventName)
+		private void Save_OnClick(object sender, RoutedEventArgs e)
+		{
+			if (Check(EventName)
 				//|| Check(EventStart) || Check(EventEnd) || Check(EventAddresse) 
 				|| Check(EventDescription))
-                return;
+				return;
 
-            if (ParentWindow.Evenement == null)
-                ParentWindow.Evenement = new SaveableEvent();
+			if (ParentWindow.Evenement == null)
+				ParentWindow.Evenement = new SaveableEvent();
 
-            ParentWindow.Evenement.Name = EventName.Text;
-            ParentWindow.Evenement.Description = EventDescription.Text;
-            //ParentWindow.Evenement.Address = EventAddresse.Text;
-           // ParentWindow.Evenement.Start = EventStart.Value.GetValueOrDefault();
-           // ParentWindow.Evenement.End = EventEnd.Value.GetValueOrDefault();
+			ParentWindow.Evenement.Name = EventName.Text;
+			ParentWindow.Evenement.Description = EventDescription.Text;
+			//ParentWindow.Evenement.Address = EventAddresse.Text;
+			// ParentWindow.Evenement.Start = EventStart.Value.GetValueOrDefault();
+			// ParentWindow.Evenement.End = EventEnd.Value.GetValueOrDefault();
 
-            Task.Run(() => Save());
-        }
+			Task.Run(() => Save());
+		}
 
-	    public void CombinedDialogOpenedEventHandler(object sender, DialogOpenedEventArgs eventArgs)
-	    {
-		    if (!(eventArgs.Session.Content is Grid grid)) return;
+		public void CombinedDialogOpenedEventHandler(object sender, DialogOpenedEventArgs eventArgs)
+		{
+			if (!(eventArgs.Session.Content is Grid grid)) return;
 
-		    if (grid.Name.Equals("StartGrid"))
-		    {
-			    CombinedCalendar.SelectedDate = ((DayPickerModel)DataContext).Start;
-			    CombinedClock.Time = ((DayPickerModel)DataContext).Start;
-			    return;
-		    }
+			if (grid.Name.Equals("StartGrid"))
+			{
+				CombinedCalendar.SelectedDate = ((DayPickerModel)DataContext).Start;
+				CombinedClock.Time = ((DayPickerModel)DataContext).Start;
+				return;
+			}
 
-		    CombinedEndCalendar.SelectedDate = ((DayPickerModel)DataContext).End;
-		    CombinedEndClock.Time = ((DayPickerModel)DataContext).End;
-	    }
+			CombinedEndCalendar.SelectedDate = ((DayPickerModel)DataContext).End;
+			CombinedEndClock.Time = ((DayPickerModel)DataContext).End;
+		}
 
-	    public void CombinedDialogClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
-	    {
-		    if (!Equals(eventArgs.Parameter, "1")) return;
-		    if (!(eventArgs.Session.Content is Grid grid)) return;
+		public void CombinedDialogClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
+		{
+			if (!Equals(eventArgs.Parameter, "1")) return;
+			if (!(eventArgs.Session.Content is Grid grid)) return;
 
-		    var start = grid.Name.Equals("StartGrid");
-		    var date = start ? CombinedCalendar.SelectedDate : CombinedEndCalendar.SelectedDate;
-		    if (date == null) return;
-		    var combined = SetTime(date.Value, start ? CombinedClock.Time : CombinedEndClock.Time);
-		    //var combined = date.Value.AddSeconds(start
-		    //   ? CombinedClock.Time.TimeOfDay.TotalSeconds
-		    //   : EndCombinedClock.Time.TimeOfDay.TotalSeconds);
+			var start = grid.Name.Equals("StartGrid");
+			var date = start ? CombinedCalendar.SelectedDate : CombinedEndCalendar.SelectedDate;
+			if (date == null) return;
+			var combined = SetTime(date.Value, start ? CombinedClock.Time : CombinedEndClock.Time);
+			//var combined = date.Value.AddSeconds(start
+			//   ? CombinedClock.Time.TimeOfDay.TotalSeconds
+			//   : EndCombinedClock.Time.TimeOfDay.TotalSeconds);
 
-		    if (FirstClose && New) // If new then the start = end date
-		    {
-			    ((DayPickerModel)DataContext).Start = ((DayPickerModel)DataContext).End = combined;
-			    FirstClose = false;
-			    return;
-		    }
+			if (FirstClose && New) // If new then the start = end date
+			{
+				((DayPickerModel)DataContext).Start = ((DayPickerModel)DataContext).End = combined;
+				FirstClose = false;
+				return;
+			}
 
-		    if (start) ((DayPickerModel)DataContext).Start = combined;
-		    else ((DayPickerModel)DataContext).End = combined;
-	    }
+			if (start) ((DayPickerModel)DataContext).Start = combined;
+			else ((DayPickerModel)DataContext).End = combined;
+		}
 
-	    public DateTime SetTime(DateTime date, DateTime time) =>
-		    new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+		public DateTime SetTime(DateTime date, DateTime time) =>
+			new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
 
 		private void Save()
-        {
-            Dispatcher.Invoke(() => { Mouse.OverrideCursor = Cursors.Wait; });
+		{
+			Dispatcher.Invoke(() => { Mouse.OverrideCursor = Cursors.Wait; });
 
-            using (var db = new CaisseServerContext())
-            {
-                db.Events.AddOrUpdate(ParentWindow.Evenement);
-                db.SaveChanges();
-            }
+			using (var db = new CaisseServerContext())
+			{
+				db.Events.AddOrUpdate(ParentWindow.Evenement);
+				db.SaveChanges();
+			}
 
-            Dispatcher.Invoke(() =>
-            {
-                Mouse.OverrideCursor = null;
-                MessageBox.Show(New ? "L'événement a bien été crée !" : "L'événement a bien été enregistré !");
-                ToggleBlocked(true);
-                Saved = true;
-            });
-        }
+			Dispatcher.Invoke(() =>
+			{
+				Mouse.OverrideCursor = null;
+				MessageBox.Show(New ? "L'événement a bien été crée !" : "L'événement a bien été enregistré !");
+				Model.Enabled = false;
+				Saved = true;
+			});
+		}
 
-        private void Blocage_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (!Saved)
-            {
-                MessageBox.Show("Veuillez enregistrer avant.");
-                Blocage.IsChecked = false;
-                return;
-            }
 
-            ToggleBlocked(false);
-            Saved = false;
-        }
+		public override void Update()
+		{
+		}
 
-        public override void Update()
-        {
-        }
+		public override void Add<T>(T item)
+		{
+		}
 
-        public override void Add<T>(T item)
-        {
-        }
+		public override bool CanClose() => Saved || !Saved && Validations.WillClose(true);
 
-        public override bool CanClose()
-        {
-            return Saved || !Saved && Validations.WillClose(true);
-        }
-
-        public override bool CanBack()
-        {
-            return Saved || Validations.WillClose(true);
-        }
-    }
+		public override bool CanBack() => Saved || Validations.WillClose(true);
+	}
 }
---
+++ CaisseDesktop/Models/EventMainPageModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CaisseServer.Events;
+
+namespace CaisseDesktop.Models
+{
+	public class EventMainPageModel : INotifyPropertyChanged
+	{
+
+		private SaveableEvent Event { get; }
+
+		public EventMainPageModel(SaveableEvent saveableEvent)
+		{
+			if (saveableEvent != null)
+			{
+				Event = saveableEvent;
+				_enabled = false;
+				return;
+			}
+
+			Event = new SaveableEvent { Start = DateTime.Now.AddDays(-1), End = DateTime.Now };
+			_enabled = true;
+		}
+
+		public bool Enabled
+		{
+			get => _enabled;
+			set
+			{
+				_enabled = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool _enabled;
+
+		public DateTime Start
+		{
+			get => Event.Start;
+			set
+			{
+				Event.Start = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public DateTime End
+		{
+			get => Event.End;
+			set
+			{
+				Event.End = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public string OwnerName
+		{
+			get => Event.AddressName;
+			set
+			{
+				Event.AddressName = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public string OwnerAddress
+		{
+			get => Event.Address;
+			set
+			{
+				Event.Address = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public string OwnerPostalCodeCity
+		{
+			get => Event.PostalCodeCity;
+			set
+			{
+				Event.PostalCodeCity = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public string OwnerSiret
+		{
+			get => Event.Siret;
+			set
+			{
+				Event.Siret = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public string OwnerTelephone
+		{
+			get => Event.Telephone;
+			set
+			{
+				Event.Telephone = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public string Name
+		{
+			get => Event.Name;
+			set
+			{
+				Event.Name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public string Description
+		{
+			get => Event.Description;
+			set
+			{
+				Event.Description = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
