74749a218fbfb6119bd69451c09aa3150c6b0e46
SchawnnDev <iperskill@gmail.com>
2019-06-18 13:58:00 +0200

    Testing server

---
+++ CaisseReservationLibrary/Handlers/ArticleReservationPacketHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Networker.Common;
+using Networker.Common.Abstractions;
+using CaisseReservationLibrary.Packets;
+
+namespace CaisseReservationLibrary.Handlers
+{
+	public class ArticleReservationPacketHandler : PacketHandlerBase<ArticleReservationPacket>
+	{
+		private readonly ILogger<ArticleReservationPacket> _logger;
+
+		public ArticleReservationPacketHandler(ILogger<ArticleReservationPacket> logger)
+		{
+			_logger = logger;
+		}
+
+		public override async Task Process(ArticleReservationPacket packet, IPacketContext packetContext)
+		{
+			_logger.LogDebug($"Checkout id={packet.CheckoutId} reserved {packet.Number} of article={packet.ArticleId}");
+			/*
+			packetContext.Sender.Send(new ArticleReservationPacket
+			{
+				Message = "Hey, I got your message!"
+			}); */
+		}
+	}
+}
--- CaisseReservationServer/Program.cs
+++ CaisseReservationServer/Program.cs
@@ -1,14 +1,49 @@
 ﻿using System;
+using System.Threading;
 using CaisseReservationLibrary;
+using CaisseReservationLibrary.Handlers;
+using CaisseReservationLibrary.Packets;
+using Microsoft.Extensions.Logging;
+using Networker.Extensions.ProtobufNet;
+using Networker.Server;
+using Networker.Server.Abstractions;
 
 namespace CaisseReservationServer
 {
-	class Program
+	public class Program
 	{
+
+		private static IServer Server;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Server = new ServerBuilder()
+				.UseTcp(5456)
+				.UseUdp(5457) // todo : config
+				.ConfigureLogging(loggingBuilder =>
+				{
+				//	loggingBuilder.AddConfiguration(config.GetSection("Logging")); make config
+					loggingBuilder.AddConsole();
+				})
+				.UseProtobufNet()
+				.RegisterPacketHandler<ArticleReservationPacket, ArticleReservationPacketHandler>()
+				.Build();
+
+			Server.Start();
+
+			while (Server.Information.IsRunning)
+			{
+				Thread.Sleep(10000);
+			}
+
+			AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
 		}
+
+		static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+		{
+			Server.Stop();
+		}
+
 	}
 }
